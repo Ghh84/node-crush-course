0c7e05efdb3da1ea7d055049e12ef975f8b397be
Ghh84 <ghiwethh@gmail.com>
2024-02-11 22:55:39 +0200

    File system module

--- reference/fs-demo.js
+++ reference/fs-demo.js
@@ -6,20 +6,25 @@ const path = require('path');
    //if (err) throw err;
    //console.log('Folder created...');
  //});
- // Create folder
-fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello nice to meet you!', err => {
-    if (err) throw err;
-    console.log('File written...');
-  });
+ // write file
+// fs.writeFile(path.join(__dirname, './', 'README.md'), '# Readme file', err => {
+//     if (err) throw err;
+//     console.log('File written...');
+//   });
   //append to
-  fs.appendFile(path.join(__dirname, '/test', 'hello.txt'), 'Lets discuse about nodejs!', err => {
-    if (err) throw err;
-    console.log('File is updated...');
-  });
+  // fs.appendFile(path.join(__dirname, '/test', 'hello.txt'), 'Lets discuse about nodejs!', err => {
+  //   if (err) throw err;
+  //   console.log('File is updated...');
+  // });
 
   // Reading from file
-  fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
+  // fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+  //   if (err) throw err;
+  //   console.log(data);
+  // });
   
+ // Rename file
+//  fs.rename(path.join(__dirname, '/test', 'hello.txt'), path.join(__dirname, '/test', 'text-file.txt'), (err) => {
+//   if (err) throw err;
+//   console.log('File renamed');
+// });
